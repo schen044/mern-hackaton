aa6946a3b2cb96650aec218cb3b2cf6f47c3b677
msiller455 <msiller455@gmail.com>
2022-11-16 13:35:00 -0800

    adds items routes and controllers

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from 'react'
+
 export default function NewOrderPage() {
-    return <h1>NewOrderPage</h1>
+    const [ menuItems, setMenuItems ] = useState([])
+
+    // this will make our useEffect run for every render
+    useEffect(function() {
+        console.log('NewOrderPage rendered')
+    })
+
+    // the useEffect will fire after the initial render
+    useEffect(function() {
+        console.log('useEffect runs only after first render')
+    }, [])
+
+    useEffect(function() {
+        console.log('useEffect runs when menuItems changes')
+    }, [menuItems])
+
+    // - Fetch the menuItems from the server via AJAX
+    // - When the data comes back, call setMenuItems to save in state
+
+
+
+    return (
+        <>
+            <h1>NewOrderPage</h1>
+            <button onClick={() => setMenuItems(Date.now())}>Trigger Rerender</button>
+        </>
+    )
 }
